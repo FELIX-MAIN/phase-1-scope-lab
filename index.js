efbfd531f835088fdba86c71e4febb7291449f7b
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-25 16:35:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+   let  frequentcustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    let frequentcustomer = 'maybe bob';
+}
+
+const LeastfrequentCustomer = "";
+
+function changeLeastFavoriteCustomer() {
+
+    console.log(LeastfrequentCustomer)
+}
+changeLeastFavoriteCustomer(LeastfrequentCustomer);
